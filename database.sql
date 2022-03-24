ebe7da10a61b038781189c5320fa70e9858af08f
Jason Inouye <jasonjinouye@gmail.com>
2022-03-24 10:57:52 -0500

    database sql doc updated with table and insert data

--- database.sql
+++ database.sql
@@ -1,2 +1,14 @@
 -- Don't forget to add your create table SQL 
 -- It is also helpful to include some test data
+-- db name shopping_cart
+CREATE TABLE cart (
+	id SERIAL PRIMARY KEY,
+	item varchar(80),
+	quantity FLOAT,
+	unit varchar(20),
+	purchased boolean DEFAULT false
+);
+
+INSERT INTO "cart" (
+	"item", "quantity", "unit"
+) VALUES ('Bread', 1, 'loaf' ), ( 'Milk', 1.5 , 'gallon');
