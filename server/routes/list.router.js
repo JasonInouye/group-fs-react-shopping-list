25ce1aebcf4a13ffb5df4071e3f473df515282e9
Freddy Reza <rezafreddy@gmail.com>
2022-03-24 13:51:06 -0500

    add inputs, buttons, GET route.

--- server/routes/list.router.js
+++ server/routes/list.router.js
@@ -4,5 +4,21 @@ const pool = require('../modules/pool.js');
 
 // TODO - Add routes here...
 
+router.get('/', (req, res) => {
+    const queryText = `
+        SELECT * FROM "cart"
+        ORDER BY UPPER ("item");
+    `;
+
+    pool.query(queryText)
+        .then((result)=> {
+            res.send(result.rows);
+        })
+        .catch((err) => {
+            res.sendStatus(500);
+        })
+});
+
+
 
 module.exports = router;
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,15 +1,58 @@
 import React from 'react';
-
+import {useState, useEffect} from 'react';
+import axios from 'axios';
 import Header from '../Header/Header.jsx'
 import './App.css';
 
-
 function App() {
+    let [shoppingList, setShoppingList] = useState([]);
+
+    useEffect(() => {
+        getList()
+    }, [])
+
+    const getList = () => {
+        console.log('In GET');
+
+        axios.get('/list').then(response => {
+            setShoppingList(response.data)
+            console.log(response.data);
+        }).catch(err => {
+            console.log(err);
+        })
+
+    }
+
+
     return (
         <div className="App">
             <Header />
             <main>
-                <p>Under Construction...</p>
+                <h1>Add an Item</h1>
+                <label htmlFor="Item">Item: </label>
+                <input type="text" placeholder="Item"/>
+                <label htmlFor="Item">Quantity: </label>
+                <input type="number" placeholder="Quantity"/>
+                <label htmlFor="Item">Unit: </label>
+                <input type="text" placeholder="Unit"/>
+                <button>ADD ITEM</button>
+
+                <h1>Shopping List</h1>
+                <button>RESET</button>
+                <button>CLEAR</button>
+
+                <div>
+                {shoppingList.map(listItem => (
+                            <div key={listItem.id}>
+                                <p>{listItem.item}</p>
+                                <p>{listItem.quantity}</p>
+                                <p>{listItem.unit}</p>
+                                <p>{listItem.purchased}</p>
+                                <button onClick={(event) => handleDelete(student.id)}>DELETE</button>
+                                <button>PURCHASE</button>
+                            </div>
+                        ))}
+                </div>
             </main>
         </div>
     );
